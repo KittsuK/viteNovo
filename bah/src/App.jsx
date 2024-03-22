90e068fea12c3ae2a2e991617afd1841646c6cec
KittsuK <pg.verissimo0802@gmail.com>
2024-03-22 14:30:06 -0300

    começando coisa nova

---
+++ bah/src/App.jsx
@@ -0,0 +1,13 @@
+import style from './App.module.css'
+
+function App() {
+  return (
+    <>
+      <div className={style['wrap-principal']}>
+        <h1>Hello world</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
